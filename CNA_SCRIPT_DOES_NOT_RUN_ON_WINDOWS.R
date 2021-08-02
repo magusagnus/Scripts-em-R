92a4cd35bdf4000414043a9928907e67ba0b7a43
Rodger Rocha <rodgerm@gmail.com>
2021-08-02 16:59:53 -0300

    Script para baixar bancos e processar metilação via firehose

---
+++ CNA_SCRIPT_DOES_NOT_RUN_ON_WINDOWS.R
@@ -0,0 +1,19 @@
+##Using RToolbox to retrieve data from Firehose 
+library(RTCGAToolbox)
+
+##Mostra as rundatas dispon?veis e ativas para serem escolhidas
+lastRunDate <- getFirehoseRunningDates()
+
+##Seleciona uma data para se fazer an?lise
+lastAnalyseDate <- getFirehoseAnalyzeDates(1)
+
+# get DNA methylation data, RNAseq2 and clinical data for GBM
+gbm.data <- getFirehoseData(dataset = "GBM",
+                            runDate = lastAnalyseDate, gistic2_Date = getFirehoseAnalyzeDates(1),
+                            Methylation = TRUE, Clinic = TRUE, RNAseq2_Gene_Norm = TRUE,
+                            fileSizeLimit = 10000)
+
+#Pega os dados que foram baixados acima
+gbm.mut <- getData(gbm.data,"Mutations")
+gbm.clin <- getData(gbm.data,"Clinical") 
+gbm.gistic <- getData(gbm.data,"GISTIC")
